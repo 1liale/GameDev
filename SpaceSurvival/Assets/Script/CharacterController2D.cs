baa79a6dcd1c77ffa8b3cc351fc35b6fde193f9d
1liale <1liale@hdsb.ca>
2020-06-28 17:30:07 -0400

    Animation
    animation + modified charactercontroller for animation

--- SpaceSurvival/Assets/Script/CharacterController2D.cs
+++ SpaceSurvival/Assets/Script/CharacterController2D.cs
@@ -5,38 +5,47 @@ using System.Collections.Generic;
 public class CharacterController2D : MonoBehaviour
 {					
 	public float speed = 6f;
+	private bool moveRight = false;
+	private bool isWalking = false;
 
 	private Rigidbody2D rigidbody;
 	private Vector2 velocity;
 	private Vector2 userInput;
 
-	[SerializeField]
-	Transform target;
+
+	public Animator animator;
 
 	void Start()
-	{	
+	{
 		rigidbody = GetComponent<Rigidbody2D>();
+		animator = GetComponent<Animator>();
 		userInput = transform.position;
 	}
 
 	void Update()
 	{
 		// When left mouse button is pressed down
-		if(Input.GetMouseButtonDown(0))
+		if (Input.GetMouseButton(0))
         {
+			animator.SetBool("walk", true);
+			isWalking = true;
 			// Stores mouse's position 
             Debug.Log("Mouse button clicked");
+			Debug.Log(animator.GetBool("walk"));
 			userInput = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition);
-			
-			// Display on inspector
-			target.position = userInput;
         }
+
+		else 
+		{
+			animator.SetBool("walk", false);
+			isWalking = false;
+		}
 	}
 
 	void FixedUpdate()
 	{
 
-		if((Vector2)transform.position != userInput)
+		if(((Vector2)transform.position != userInput) && isWalking)
     	{
        		transform.position = Vector2.MoveTowards(transform.position, userInput, speed * Time.deltaTime);
     	}
