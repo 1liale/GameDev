b51675976673d9d71456146c0df5102d861b4188
1liale <1liale@hdsb.ca>
2020-06-30 17:54:31 -0400

    Newest changes
    Programmed button functions and reorganized files

---
+++ SpaceSurvival/Assets/Scripts/Script/ButtonHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonHandler : MonoBehaviour
+{
+    public GameObject pointer1;
+    public GameObject pointer2;
+
+    public void onHover()
+    {
+        pointer1.GetComponent<SpriteRenderer>().enabled = true;
+    }
+
+    public void offHover()
+    {
+        pointer1.GetComponent<SpriteRenderer>().enabled = false;
+    }
+
+    public void onClick()
+    {
+        pointer1.GetComponent<SpriteRenderer>().enabled = false;
+        pointer2.GetComponent<SpriteRenderer>().enabled = true;
+    }
+}
