afd3a18f0d23f6b2ee0b916e0405bcf4f184cebc
1liale <1liale@hdsb.ca>
2020-06-30 13:55:10 -0400

    Update Master
    Could be problematic

---
+++ SpaceSurvival/Assets/IntroScripts/Intro/ButtonHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonHandler : MonoBehaviour
+{
+    
+    public GameObject pointerButton1;
+    public GameObject pointerButton2;
+    public void onHover()
+    {
+        pointerButton1.GetComponent<SpriteRenderer>().enabled = true;
+    }
+
+    public void offHover()
+    {
+        pointerButton1.GetComponent<SpriteRenderer>().enabled = false;
+    }
+
+    public void buttonClicked()
+    {
+        pointerButton1.GetComponent<SpriteRenderer>().enabled = false;
+        pointerButton2.GetComponent<SpriteRenderer>().enabled = true;
+    }
+}
