2478f97bdcadc848b036a6b28d9c4dc708b94113
Joey Zhou <51102374+moderatesaltiness@users.noreply.github.com>
2020-06-29 23:54:37 -0400

    DialogueManager
    pretty basic

---
+++ SpaceSurvival/Assets/Scripts/Dialogue.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+public class Dialogue : MonoBehaviour
+{
+    private Tuple<string, string>[] lines;
+    private int index;
+
+    public void Reset()
+    {
+        index = 0;
+    }
+    
+    public void Load()
+    {
+        
+    }
+
+    public Tuple<string, string> nextLine()
+    {
+        if (index >= lines.Length)
+            return null;
+        return lines[++index];
+    }
+
+}
