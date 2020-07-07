87eaee6b7328d4163b11f525f8e924a5c2ef3ea2
Ray Wang <raywang999@gmail.com>
2020-07-06 22:27:48 -0400

    Documentation

--- SpaceSurvival/Assets/Scripts/Script/ButtonHandler.cs
+++ SpaceSurvival/Assets/Scripts/Script/ButtonHandler.cs
@@ -3,21 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+///<summary>Class <c>ButtonHandler</c> contains functions giving buttons their functionality.</summary>
 public class ButtonHandler : MonoBehaviour
 {
+    ///Object containing sprite to appear on hover
     public GameObject pointer1;
+    ///Object containing sprite to appear on click
     public GameObject pointer2;
-
+    ///Behavior for pointer hover over button 
     public void onHover()
     {
         pointer1.GetComponent<SpriteRenderer>().enabled = true;
     }
 
+    ///Behavior for pointer move away from button 
     public void offHover()
     {
         pointer1.GetComponent<SpriteRenderer>().enabled = false;
     }
-
+    ///Behavior 
     public void onSwipe()
     {
         pointer1.GetComponent<SpriteRenderer>().enabled = false;
