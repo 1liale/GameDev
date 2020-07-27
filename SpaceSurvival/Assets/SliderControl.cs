56c3f7098d2570fc492cc20a81475dab9f25daae
1liale <1liale@hdsb.ca>
2020-07-27 19:25:23 -0400

    options menu completed
    completed a basic options menu to adjust music volume

---
+++ SpaceSurvival/Assets/SliderControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderControl : MonoBehaviour
+{
+    private static Slider slider;
+    private AudioSource backgroundMusic;
+    // Start is called before the first frame update
+    void Start()
+    {
+        slider = GetComponent<Slider>();
+        slider.gameObject.SetActive(false);
+        backgroundMusic = GameObject.Find("BackgroundAudio").GetComponent<AudioSource>();
+        slider.value = backgroundMusic.volume;
+    }
+
+    // Update is called once per frame
+    public static void setVisible()
+    {
+        slider.gameObject.SetActive(true);
+    }
+
+    void Update()
+    {
+        backgroundMusic.volume = slider.value;
+    }
+}
