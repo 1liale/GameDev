24ccbb2f55de73d8ba0c8c1f9d1f830861172b17
Joey Zhou <51102374+moderatesaltiness@users.noreply.github.com>
2020-07-31 16:25:37 -0400

    Update ObjectCollision.cs

--- SpaceSurvival/Assets/Scripts/Script/ObjectCollision.cs
+++ SpaceSurvival/Assets/Scripts/Script/ObjectCollision.cs
@@ -5,17 +5,26 @@ using UnityEngine;
 public class ObjectCollision : MonoBehaviour
 {
     private Rigidbody2D rigidbody;
-    public Animator anim;
+    private Animator anim;
     void Start()
     {
         rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Door")
+        {
+            Debug.Log("Door zone entered");
+            anim = collider.GetComponent<Animator>();
+        }
     }
 
     void OnTriggerStay2D(Collider2D collider)
     {
         if(collider.gameObject.tag == "Door")
         {
-             Debug.Log("Door zone entered");
+            Debug.Log("Door zone entered");
             if(Input.GetKey(KeyCode.H))
             {
                 Debug.Log("Door open");
